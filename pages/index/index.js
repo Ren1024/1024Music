b4d45f8caff2805c1f90f3155bf6b97e0c533f73
chumen <582831743@qq.com>
2022-09-14 23:16:02 +0800

    推荐歌曲完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,79 +1,63 @@
 // pages/index/index.js
+import request from '../../utils/request'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+  //页面的初始数据
   data: {
-    banners:[]
+    banners:[], //轮播图
+    recommendList:[]  //推荐歌曲列表
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  //生命周期函数--监听页面加载
   onLoad(options) {
     // 发请求获取banner
-    wx.request({
-      url:'http://localhost:3000/banner',
-      data:{type: 2},
-      success: (res) => {
-        // console.log('成功', res);
-        console.log('成功', res.data);
-        this.setData({
-          banners: res.data.banners
-        })
-      },
-      fail: (err) => {
-        console.log('失败', err);
-      }
+    this.getInitData()
+  },
+  // 页面初始化请求数据
+  async getInitData(){
+    // 请求获取轮播图
+    let result = await request('/banner', {type: 2})
+    this.setData({
+      banners: result.banners
+    })
+    // 请求获取歌曲列表
+    result = await request('/personalized')
+    this.setData({
+      recommendList: result.result
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  //生命周期函数--监听页面初次渲染完成
   onReady() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  // 生命周期函数--监听页面显示
   onShow() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+  //生命周期函数--监听页面隐藏
   onHide() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+  //生命周期函数--监听页面卸载
   onUnload() {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  //页面相关事件处理函数--监听用户下拉动作
   onPullDownRefresh() {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+  //页面上拉触底事件的处理函数
   onReachBottom() {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
+  //用户点击右上角分享
   onShareAppMessage() {
 
   }
---
+++ utils/request.js
@@ -0,0 +1,31 @@
+// 封装发送ajax请求的功能函数
+/* 
+  1、封装功能函数
+    1）功能点明确
+    2）函数内部保留固定的代码（静态代码）
+    3）将动态数据抽取出来，由使用者提供最终的数据，以形参的形式提供
+    4）一个良好的功能函数应该设置形参的默认值
+  2、封装功能组件
+    1）功能点明确
+    2）组件内部应该保留静态代码
+    3）将动态的数据提取成props参数，由使用者提供最终的数据
+    4)一个良好的组件应该设置组件props数据的必要性及数据类型
+*/
+import config from './config'
+export default function(url, data={}, method='GET'){
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: config.host + url,
+      data,
+      method,
+      success: (res) => {
+        // console.log('成功',res.data);
+        resolve(res.data)
+      },
+      fail: (err) => {
+        // console.log('失败',err);
+        reject(err)
+      }
+    })
+  })
+}
