e170a53d72c41ff07bd135a1e26d7bd35c9dfff2
chumen <582831743@qq.com>
2022-09-21 18:32:18 +0800

    'songDetail-start'

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,7 +1,6 @@
 import request from '../../utils/request'
 // pages/video/video.js
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -10,7 +9,9 @@ Page({
     currentId:'',
     videoList:[], 
     videoId:'', //当前播放视频id
-    videoTime: [] //记录视频播放时间
+    videoTime: [], //记录视频播放时间,
+    isTriggered:false, //是否下拉刷新
+    offset: 0, //视频分页参数
   },
 
   /**
@@ -24,13 +25,15 @@ Page({
   async getNavGroupList(){
     let result = await request('/video/group/list')
     // console.log(result);
+    // 加工数据
+    let newNavList = result.data.slice(0, 14)
     // 更新数据
     this.setData({
-      navList: result.data.slice(0, 14),
-      currentId: result.data[0].id
+      navList: newNavList,
+      currentId: newNavList[0].id
     })
     // 拿到导航id后，获取视频列表
-    this.getVideoGroupList(this.data.currentId)
+    this.getVideoGroupList()
   },
 
   // 点击切换导航标题
@@ -39,7 +42,7 @@ Page({
     wx.showLoading({
       title: '正在加载',
     })
-    let currentId = event.currentTarget.id
+    let currentId = event.currentTarget.id.slice(3)
     // console.log(event);
     this.setData({
       // 向右位移0位，转化成number类型
@@ -47,23 +50,33 @@ Page({
       // 清空上次的视频列表数据
       videoList: []
     })
-    this.getVideoGroupList(currentId)
+    this.getVideoGroupList()
   },
   
   // 获取视频
-  async getVideoGroupList(currentId){
-    let index = 0
-    // let result = await request('/video/group', {id: currentId})
-    let result = await request('/video/timeline/recommend')
+  async getVideoGroupList(newOffset = 0){
+    // 获取数据
+    let {videoList, offset} = this.data
+    // 发请求拿数据
+    let result = await request('/video/timeline/recommend', {offset: newOffset})
+    // 加工数据，添加每个对象id
+    let newVideoList = result.datas.map(item => {
+      item.id = item.data.vid
+      return item
+    })
+    if(newOffset > 0){
+      videoList.push(...newVideoList)
+    }else {
+      videoList = newVideoList
+    }
+    offset = newOffset
     // 关闭loding
     wx.hideLoading()
-    // console.log(result);
     // 更新数据
     this.setData({
-      videoList: result.datas.map(item => {
-        item.id = index++
-        return item
-      })
+      videoList,
+      offset,
+      isTriggered: false
     })
   },
 
@@ -94,10 +107,14 @@ Page({
     // 创建video的实例对象
     this.VideoContext = wx.createVideoContext(vid)
     // 判断视频是否播放过，如果是，跳转到上次位置
+    // 获取记录时长数据
     let {videoTime} = this.data
-    
-    this.VideoContext.seek()
-
+    // 判断是否有播放记录
+    let videoTimeItem = videoTime.find(item => item.vid === vid)
+    // 如果有，跳转到指定播放记录
+    if(videoTimeItem){
+      this.VideoContext.seek(videoTimeItem.currentTime)
+    }
     this.VideoContext.play()
     
   },
@@ -130,6 +147,29 @@ Page({
       videoTime
     })
 
+  },
+
+  // 视频播放完成的回调
+  handleEnded(event){
+    // 获取记录视频时长对象
+    let {videoTime} = this.data
+    // 通过findIndex取到下标，再通过splice删除
+    videoTime.slipce(videoTime.findIndex(item => item.vid === event.currentTarget.id), 1)
+  },
+
+  //下拉刷新回调
+  hindleRefresh(){
+    // console.log('scroll-view下拉刷新');
+    // 重新发送请求获取视频
+    this.getVideoGroupList()
+  },
+
+  // 滑动触底，加载更多
+  async handleTolower(){
+    // console.log('滑动触底了');
+    let {offset} = this.data
+    offset++
+    this.getVideoGroupList(offset)
   },
 
   /**
@@ -177,7 +217,20 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
+  onShareAppMessage({from}) {
+    console.log(from);
+    if(from === 'button'){
+      return {
+        title:'button转发内容',
+        path:'/pages/video/video.js',
+        imageUrl:'/static/images/nvsheng.jpg'
+      }
+    }else {
+      return {
+        title:'menu转发内容',
+        path:'/pages/video/video.js',
+        imageUrl:'/static/images/nvsheng.jpg',
+      }
+    }
   }
 })
